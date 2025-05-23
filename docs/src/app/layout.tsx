c46a173f126926772ca021a553088ebdfca59197
Daniel Luna <git@danielluna.nyc>
2025-05-23 14:41:18 -0500

    First attempt at navbar

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app/appsidebar";
 import { AppNavbar } from "@/components/app/appnavbar";
 
@@ -20,12 +20,22 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <SidebarProvider>
-        <AppSidebar/>
-        <main>
-          <AppNavbar/>
-          <SidebarTrigger />
-          {children}
-        </main>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b">
+            <div className="flex h-14 w-full items-center gap-2 px-4">
+              <SidebarTrigger />
+              <AppNavbar />
+              <div className="ml-auto flex items-center gap-2">
+                <p>Right</p>
+              </div>
+            </div>
+          </header>
+
+          <main>
+            {children}
+          </main>
+        </SidebarInset>
         </SidebarProvider>
       </body>
     </html>
