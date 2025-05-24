058ff6d4aa49f96f29802e850ff56205d50f0af6
Daniel Luna <git@danielluna.nyc>
2025-05-24 07:27:12 -0500

    Add testing library support

--- src/common/api/database.cpp
+++ src/common/api/database.cpp
@@ -6,6 +6,8 @@ namespace KoalaDB {
 KoalaDB::KoalaDB(const char* path) {
   if (path == nullptr) {
     printf("Creating In-Memory DB\n");
+  } else {
+    printf("Creating a persistant DB: %s\n", path);
   }
 }
 
---
+++ test/parser/simple.cpp
@@ -0,0 +1,3 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Empty SQL Statement", "[SQL_PARSER]") { REQUIRE(1 == 1); }
